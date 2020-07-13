0ecc8dbe82995a3ae2d676cc2baaa6c3f82876db
zischler <tobias.zischler@gmx.ch>
2020-07-13 14:40:04 +0200

    - changed anim component logic and transition

--- frontend/application/src/components/scripts/anim-component.ts
+++ frontend/application/src/components/scripts/anim-component.ts
@@ -1,8 +1,12 @@
-import {Component, Vue} from "vue-property-decorator";
+import {Component, Prop, Vue} from "vue-property-decorator";
 import verticalState, { VerticalState } from "../../helpers/vertical-state";
 
 @Component
 export default class AnimComponent extends Vue {
+    @Prop({type: Boolean, default: false})
+    hasImage!: boolean;
+
+    loadedImage: boolean = !this.hasImage;
     animate: boolean = false;
     topProgress: number = 0;
 
@@ -14,7 +18,7 @@ export default class AnimComponent extends Vue {
     checkViewPort(component: HTMLElement) {
         const VState: VerticalState = verticalState(0,0)(component);
         this.topProgress = VState.topProgress;
-        if(this.topProgress > 0.1) {
+        if(this.topProgress > 0.1 && this.loadedImage) {
             this.animate = true;
         } else {
             setTimeout(() => this.checkViewPort(component), 100);
