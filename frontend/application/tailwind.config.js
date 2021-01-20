17e1a02b54ca28119fa58b599104b630231bd40f
zischler <tobias.zischler@gmx.ch>
2021-01-20 14:03:41 +0100

    - removed quote styling - cleaned article styling - cleaned variables.css - added Roboto Font to tailwind config
    Signed-off-by: zischler <tobias.zischler@gmx.ch>

--- frontend/application/tailwind.config.js
+++ frontend/application/tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Roboto', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      'serif': ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+    },
     extend: {
       flex: {
         '1of1': '0 1 100%',
