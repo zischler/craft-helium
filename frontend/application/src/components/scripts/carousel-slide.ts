0c22caa2b5370aa2cb18588eb465b3ebab94acbf
zischler <tobias.zischler@gmx.ch>
2021-01-29 14:02:56 +0100

    - fixed carousel-height calculation without ratio
    Signed-off-by: zischler <tobias.zischler@gmx.ch>

--- frontend/application/src/components/scripts/carousel-slide.ts
+++ frontend/application/src/components/scripts/carousel-slide.ts
@@ -15,22 +15,21 @@ export default class CarouselSlide extends Vue.with(Props) {
     @Action("getCarouselHeight") getCarouselHeight;
 
     mounted() {
-        const height = this.calcHeight();
-        const carouselMetadata = {
-            id: this.carouselId,
-            height
-        } as CarouselMetadata;
+        window.requestAnimationFrame(() => {
+            const height = this.calcHeight();
+            const carouselMetadata = {
+                id: this.carouselId,
+                height
+            } as CarouselMetadata;
 
-        this.getCarouselHeight(this.carouselId).then(carouselHeight => {
-            if(carouselHeight < height) {
-                this.increaseCarouselHeight(carouselMetadata);
-            }
-        })
+            this.getCarouselHeight(this.carouselId).then(carouselHeight => {
+                if(carouselHeight < height) {
+                    this.increaseCarouselHeight(carouselMetadata);
+                }
+            })
+        });
     }
 
-    // TODO: Fix Calculation of image and video height.
-    // Currently doesn't find some images and videos to get their height and width.
-    // Maybe needs a requestAnimationFrame, which would then affect the carousel if added.
     calcHeight(): number {
         const calcHeightImg = this.calcHeightOfQuery("img");
         const calcHeightVideo = this.calcHeightOfQuery("video");
@@ -41,7 +40,7 @@ export default class CarouselSlide extends Vue.with(Props) {
             const width = mediaElement.width;
             calcHeight = this.$el.scrollWidth / (width / height);
         }
-        return calcHeight === 0 ? this.$el.scrollWidth : calcHeight;
+        return calcHeight;
     }
 
     calcHeightOfQuery(query): number {
@@ -50,20 +49,5 @@ export default class CarouselSlide extends Vue.with(Props) {
             return element.scrollHeight;
         }
         return 0;
-    }
-
-    /* TODO Remove me if not needed */
-    findLazyMediaComponent() {
-
-        return this.$el.querySelector("div[type.name=LazyMedia]");
-        /*if(this.$slots) {
-            const children = this.$slots.default();
-            for(let child of children) {
-                if(this.isLazyMediaComponent(child)) {
-                    return child;
-                }
-            }
-        }
-        return null;*/
     }
 }
